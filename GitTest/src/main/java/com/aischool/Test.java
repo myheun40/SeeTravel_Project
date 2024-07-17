77fbdbe78b844d6f47f24dc8edc71b298a6e7898
USER <USER@DESKTOP-8MDGR26K>
2024-07-17 16:31:17 +0900

    commit

--- GitTest/src/main/java/com/aischool/Test.java
+++ GitTest/src/main/java/com/aischool/Test.java
@@ -14,9 +14,6 @@ import javax.servlet.http.HttpServletResponse;
 public class Test extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.print("첫 번째 코드 생성");
 	}
