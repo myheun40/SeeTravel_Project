09743f3cf3b623aea0a638b52600c3f1e914b3b2
C:\Users\USER\git\Travel <jun10202121@naver.com>
2024-07-25 14:16:42 +0900

    .

---
+++ GitTest/src/main/java/com/aischool/service/CheckEmailServlet.java
@@ -0,0 +1,35 @@
+package com.aischool.service;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.aischool.model.WebMemberDAO;
+
+/**
+ * Servlet implementation class CheckEmailServlet
+ */
+@WebServlet("/CheckEmailServlet")
+public class CheckEmailServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		 String email = request.getParameter("email");
+	        WebMemberDAO dao = new WebMemberDAO();
+	        
+	        boolean isEmailExists = dao.checkEmailExists(email);
+	        
+	        response.setContentType("text/plain");
+	        response.setCharacterEncoding("UTF-8");
+	        
+	        if (isEmailExists) {
+	            response.getWriter().write("이미 존재하는 이메일입니다.");
+	        } else {
+	            response.getWriter().write("사용가능한 이메일입니다.");
+	        }
+	}
+
+}
